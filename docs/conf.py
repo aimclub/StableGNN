46a827889b06ba5adcad64656ba96ead48261056
Danila Vaganov <danwhale@MacBook-Pro-Danila.local>
2022-12-12 11:18:54 +0300

    autodic

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@ author = 'NCCR Team (ITMO University)'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'tests', 'tutorials']
@@ -23,5 +25,5 @@ autodoc_mock_imports = ["torch", "torch_geometric", "bamt", "optuna", "pgmpy"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
