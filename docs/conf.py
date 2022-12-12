e9232fedc03cca56fe97d799909d8c03365b46d0
Danila Vaganov <danwhale@MacBook-Pro-Danila.local>
2022-12-12 11:23:14 +0300

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'tests', 'tutorials']
-autodoc_mock_imports = ["torch", "torch_geometric", "bamt", "optuna", "pgmpy"]
+autodoc_mock_imports = ["torch", "torch_geometric", "bamt", "optuna", "pgmpy", "sklearn", "pandas", "scipy"]
 
 
 # -- Options for HTML output -------------------------------------------------
