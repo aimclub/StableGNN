cad2fbffd91f1a50709b555d65ad6d182c3491d6
Danila Vaganov <danwhale@MacBook-Pro-Danila.local>
2022-12-12 10:47:07 +0300

    update reqs

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'tests', 'tutorials']
-autodoc_mock_imports = ["torch", "torch_geometric"]
+autodoc_mock_imports = ["torch", "torch_geometric", "bamt", ""]
 
 
 # -- Options for HTML output -------------------------------------------------
