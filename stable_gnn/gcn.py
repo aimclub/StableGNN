84bdb7f21b6727454b1f2e40f90c9e3fa68a9064
Paul Rock <paul@drteam.rocks>
2023-06-24 12:51:20 +0300

    GCN model implmented

---
+++ stable_gnn/gcn.py
@@ -0,0 +1,16 @@
+import torch
+from torch_geometric.nn import GCNConv
+
+
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv2(x, edge_index)
+        return torch.sigmoid(x)
+
