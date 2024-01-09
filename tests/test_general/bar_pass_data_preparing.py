b43f559d8199dad90376d798b1ffa4881243e235
GrigoriJasnovidov <griga1995@yandex.ru>
2024-01-09 22:26:54 +0300

    linter6

--- tests/test_general/bar_pass_data_preparing.py
+++ tests/test_general/bar_pass_data_preparing.py
@@ -18,18 +18,18 @@ def grouper_gpa(x):
         return 1
 
 
-def prepare_data(name='bar_pass_prediction'):
-    if name == 'bar_pass_prediction':
-        d = pd.read_csv('bar_pass_prediction.csv')
-        for x in ['ID', 'race1', 'race2', 'sex', 'bar', 'dnn_bar_pass_prediction', 'pass_bar', 'indxgrp2',
-                  'gender', 'grad', 'Dropout', 'fulltime', 'lsat', 'zfygpa', 'ugpa', 'zgpa', 'other', 'asian',
-                  'black', 'hisp']:
+def prepare_data(name="bar_pass_prediction"):
+    if name == "bar_pass_prediction":
+        d = pd.read_csv("bar_pass_prediction.csv")
+        for x in ["ID", "race1", "race2", "sex", "bar", "dnn_bar_pass_prediction", "pass_bar", "indxgrp2",
+                  "gender", "grad", "Dropout", "fulltime", "lsat", "zfygpa", "ugpa", "zgpa", "other", "asian",
+                  "black", "hisp"]:
             del d[x]
 
-        d['gpa'] = d['gpa'].apply(grouper_gpa)
-        d['race'] = d['race'].apply(grouper_race)
-        d = d.rename(columns={'gpa': 'target', 'race': 'attr'})
+        d["gpa"] = d["gpa"].apply(grouper_gpa)
+        d["race"] = d["race"].apply(grouper_race)
+        d = d.rename(columns={"gpa": "target", "race": "attr"})
         d = pd.get_dummies(d, drop_first=True)
-        d = d.dropna(how='any')
+        d = d.dropna(how="any")
 
         return d
