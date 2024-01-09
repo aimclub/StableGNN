1b9f143db67223eb90b05d35483cb8b5b8f25ecb
GrigoriJasnovidov <griga1995@yandex.ru>
2024-01-09 22:20:29 +0300

    linter5

--- tests/test_general/bar_pass_data_preparing.py
+++ tests/test_general/bar_pass_data_preparing.py
@@ -5,7 +5,7 @@ import pandas as pd
 def grouper_race(x):
     if x == 7:
         return 1
-    else: 
+    else:
         return 0
 
 
@@ -19,17 +19,16 @@ def grouper_gpa(x):
 
 
 def prepare_data(name='bar_pass_prediction'):
-
     if name == 'bar_pass_prediction':
         d = pd.read_csv('bar_pass_prediction.csv')
         for x in ['ID', 'race1', 'race2', 'sex', 'bar', 'dnn_bar_pass_prediction', 'pass_bar', 'indxgrp2',
-                 'gender', 'grad', 'Dropout', 'fulltime', 'lsat', 'zfygpa', 'ugpa', 'zgpa', 'other', 'asian',
-                 'black', 'hisp']:
+                  'gender', 'grad', 'Dropout', 'fulltime', 'lsat', 'zfygpa', 'ugpa', 'zgpa', 'other', 'asian',
+                  'black', 'hisp']:
             del d[x]
 
         d['gpa'] = d['gpa'].apply(grouper_gpa)
         d['race'] = d['race'].apply(grouper_race)
-        d = d.rename(columns = {'gpa': 'target', 'race': 'attr'})
+        d = d.rename(columns={'gpa': 'target', 'race': 'attr'})
         d = pd.get_dummies(d, drop_first=True)
         d = d.dropna(how='any')
 
